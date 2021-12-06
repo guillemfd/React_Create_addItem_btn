81a191cc60fb54e9e1c964ba99019adac9a580b9
Guillem Ferrer <guillemfd@gmail.com>
2021-12-06 23:52:35 +0100

    Card & Delete btn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+//Components
+import Product from './Product.jsx'
+
+
 
 function App() {
+
+  const [user, setUser] = useState({
+    username: "guillemfd",
+    name: "Guillem",
+    lastname: "Ferrer",
+    admin: true,
+    products: [
+      {
+        name: `Televisión 42"`,
+        description: "Televisión en perfecto estado, con 6 meses de garantía.",
+        category: "TV, Audio y Foto",
+        interests: "Móviles y Telefonía",
+      },
+      {
+        name: "Bici carretera",
+        description: "Bici con ruedas de 700mm, 2 años de uso, mantenimiento al día, buen estado general",
+        category: "Deporte y Ocio",
+        interests: "Móviles y Telefonía",
+      },
+      {
+        name: "Chaqueta hombre",
+        description: "Chaqueta marrón de entretiempo, talla L, con forro interior.",
+        category: "Moda y accesorios",
+        interests: "Móviles y Telefonía",
+      },
+      {
+        name: "Cámara Canon 800D",
+        description: "Cámara réflex con unos 4.000 disparos, adquirida en Agosot de 2019.",
+        category: "TV, Audio y Foto",
+        interests: "Móviles y Telefonía",
+      },
+    ],
+  });
+
+  const deleteProductAndUpdate = (productName)=>{
+    const filteredProducts = user.products.filter((product) => {
+      return productName !== product.name;
+    })
+    //como sólo queremos modificar una cosa del usuario, tenemos que hacer lo siguiente (si quisieramos modificar todo el usuario funcionaria sólo con la línea 52). Creamos una copia del array original de user y modificamos ese (el estado original no lo podemos modificar)
+    const copyOfUser = {...user}
+    copyOfUser.products = filteredProducts
+
+    setUser(copyOfUser);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Adding items</h1>
+      <div className="cards">
+        {user.products.map((product, index)=>{
+        return <Product key={index + Date.now()} productData={product} funcionBorrar={deleteProductAndUpdate} usuario={user.username}/>
+        })}
+      </div>
+
     </div>
   );
 }
---
+++ src/Product.jsx
@@ -0,0 +1,60 @@
+import "./Product.css";
+import React from 'react';
+
+const Product = (props)=>{
+    const {name, description, category, interests} = props.productData
+
+    const getCategory = () => {
+        if(category === "TV, Audio y Foto"){
+          return "📽"
+        } else if(category === "Deporte y Ocio"){
+          return "🤸‍♂️"
+        } else if (category === "Moda y accesorios"){
+          return "👕"
+        } else if (category === "Móviles y Telefonía"){
+            return "📱"
+        } else {
+        return "";
+        };
+    }
+    
+    const getBackground = () => {
+        if(category === "TV, Audio y Foto"){
+          return "rgb(210, 230, 255)"
+        } else if(category === "Deporte y Ocio"){
+          return "#daffd2"
+        } else if (category === "Moda y accesorios"){
+          return "#ffd2d2"
+        } else if (category === "Móviles y Telefonía"){
+            return "#d9d2ff"
+        } else {
+        return "";
+        };
+    }
+
+    const getInterests = () => {
+        if(interests === "TV, Audio y Foto"){
+          return "📽"
+        } else if(interests === "Deporte y Ocio"){
+          return "🤸‍♂️"
+        } else if (interests === "Moda y accesorios"){
+          return "👕"
+        } else if (interests === "Móviles y Telefonía"){
+            return "📱"
+        } else {
+        return "";
+        };
+    }
+    
+
+    return (
+        <div className="Product" style={{backgroundColor: getBackground()}}>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <p>El usuario {props.usuario} tiene un objecto de la categoría {getCategory()} y está interesado en cambiarlo por otro de {getInterests()}</p>
+            <button onClick={()=>props.funcionBorrar(name)}>Borrar</button>
+        </div>
+    );
+};
+
+export default Product;
